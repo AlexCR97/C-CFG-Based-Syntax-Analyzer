b570330d448f75e2a4a3b541fe2be1e32fa14adf
AlexCR97 <carp_97@outlook.com>
2019-05-27 23:06:58 -0500

    Initial Commit

---
+++ Earley/MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace CFGParser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonParse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(553, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(514, 473);
+            this.textBox1.TabIndex = 0;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(534, 506);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // buttonParse
+            // 
+            this.buttonParse.Location = new System.Drawing.Point(553, 495);
+            this.buttonParse.Name = "buttonParse";
+            this.buttonParse.Size = new System.Drawing.Size(514, 23);
+            this.buttonParse.TabIndex = 2;
+            this.buttonParse.Text = "Parse";
+            this.buttonParse.UseVisualStyleBackColor = true;
+            this.buttonParse.Click += new System.EventHandler(this.buttonParse_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 531);
+            this.Controls.Add(this.buttonParse);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button buttonParse;
+    }
+}
---
+++ Earley/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CFGParser
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Test();
+        }
+
+        private void Test()
+        {
+            string[] tokens1 = { "if", "(", "var9", "==", "3", ")", "{", "string", "var5", ";", "}" };
+            string[] tokens2 = { "if", "(", "var9", "<=", "3", ")", "{", "bool", "var5", "=", "var2", ";", "}" };
+            string[] tokens3 = { "if", "(", "var5", ">=", "var1", ")", "{", "char", "var1", "=", "4", ";", "}" };
+            string[] tokens4 = { "if", "(", "var5", "!=", "var8", ")", "{", "int", "var1", "=", "var0", "(", ")", ";", "}" };
+            string[] tokens5 = { "if", "(", "var0", "<", "5", ")", "{", "double", "var7", "=", "var0", ".", "var9", "(", ")", ";", "}" };
+
+            string[] error1 = { "var9", "==", "3", "then", "string", "var5", ";", "end" };
+            string[] error2 = { "if", "var10", "<=", "3", "then", "bool", "var5", "=", "var2", ";", "end" };
+            string[] error3 = { "if", "var5", ">=", "var1", "then", "char", "=", "4", ";", "end" };
+            string[] error4 = { "if", "var5", "!=", "var8", "then", "int", "var1", "=", "var0", "(", ")", "end" };
+            string[] error5 = { "if", "var0", "<", "5", "then", "double", "var7", "=", "var0", ".", "var9", "(", ")", ";" };
+
+            var grammar = new AleGrammar();
+            var parser = new Parser(grammar);
+
+            Parse(tokens1, parser, false);
+            Parse(tokens2, parser, false);
+            Parse(tokens3, parser, false);
+            Parse(tokens4, parser, false);
+            Parse(tokens5, parser, false);
+
+            Parse(error1, parser, false);
+            Parse(error2, parser, false);
+            Parse(error3, parser, false);
+            Parse(error4, parser, false);
+            Parse(error5, parser, false);
+        }
+
+        private void Parse(string[] tokens, Parser parser, bool clear)
+        {
+            if (clear)
+                textBox1.Clear();
+
+            var output = new StringBuilder();
+
+            for (int i = 0; i < tokens.Length - 1; i++)
+                output.Append(tokens[i] + " ");
+
+            string sentence = output.ToString();
+            textBox1.Text += "Parsing: " + sentence + "\r\n";
+
+            bool success = parser.parseSentence(tokens);
+
+            if (success)
+                textBox1.Text += "Parse SUCCESSFUL! :D\r\n";
+            else
+                textBox1.Text += "Parse FAILED :(\r\n";
+
+            Chart[] charts = parser.getCharts();
+            textBox1.Text += "Charts produced by the sentence: " + sentence + "\r\n";
+
+            for (int i = 0; i < charts.Length; i++)
+            {
+                textBox1.Text += "Chart[" + i.ToString() + "] :\r\n";
+                textBox1.Text += charts[i].ToString() + "\r\n";
+            }
+
+            textBox1.Text += "========================================================";
+            textBox1.Text += "\r\n";
+            textBox1.Text += "========================================================";
+            textBox1.Text += "\r\n";
+        }
+
+        private void buttonParse_Click(object sender, EventArgs e)
+        {
+            string input = richTextBox.Text;
+            string[] tokens = input.Split(null);
+
+            var grammar = new AleGrammar();
+            var parser = new Parser(grammar);
+
+            Parse(tokens, parser, true);
+        }
+    }
+}
